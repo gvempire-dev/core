531f15d8e74712491ec62c0ebee6980a1b066dc5
KJ <kj@gvempire.com>
2021-05-20 21:22:37 -0400

    feat(build): init docusaurus build

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,50 +1,50 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'GVEMPIRE',
+  tagline: 'Good Vibes and Greater Ventures',
+  url: 'https://gvempire.dev',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  favicon: 'icons/favicon.ico',
+  organizationName: 'gvempire-dev', // Usually your GitHub org/user name.
+  projectName: 'GVEMPIRE', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'GVEMPIRE',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'GVEMPIRE Logo',
+        src: 'logos/logo__dark.png',
       },
       items: [
         {
           type: 'doc',
-          docId: 'intro',
+          docId: 'home',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Home',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          type: 'doc',
+          docId: 'about',
+          position: 'left',
+          label: 'About',
         },
+        {
+          type: 'doc',
+          docId: 'services',
+          position: 'left',
+          label: 'Services',
+        },
+        { to: '/case-studies', label: 'Case Studies', position: 'right' },
+        { to: '/gallery', label: 'Gallery', position: 'right' },
+        { to: '/blog', label: 'Blog', position: 'right' },
       ],
     },
     footer: {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
+          title: 'Follow Me!',
           items: [
             {
               label: 'Stack Overflow',
